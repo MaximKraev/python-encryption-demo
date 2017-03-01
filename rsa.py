86da9ec8d457615decbfc78b5c4b5065ac4c6944
Maksim Kraev <maxim.kraev@gmail.com>
2017-03-01 10:09:06 +0000

    fixes in rsa

--- rsa.py
+++ rsa.py
@@ -5,10 +5,12 @@ import ast
 from Crypto import Random
 from Crypto.PublicKey import RSA
 
+CRYPT_BLOCK_SIZE = 128
+
 def usage():
   print """Usage:
      rsa.py keys <private.key> <public.key>
-     rsa.py encrypt public.pem inputFile <outputFile>
+     rsa.py encrypt public.pem inputFile outputFile
      rsa.py decrypt private.pem inputFile <outputFile>
      """
   exit(1)
@@ -28,7 +30,7 @@ def keys(filenames):
   write_file(private_key_filename, key.exportKey())
 
 def encrypt(args):
-  if len(args) < 2:
+  if len(args) < 3:
     usage()
 
   public_key_file = args[0]
@@ -36,13 +38,16 @@ def encrypt(args):
 
   public_key = read_file(public_key_file)
   key = RSA.importKey(public_key)
-  encrypted_text = key.encrypt(read_file(source_file), 1)
-  encoded_text = _encode(str(encrypted_text))
+  text_to_encrypt = read_file(source_file)
+  encrypted_text = ''
+  while True:
+    chunk = text_to_encrypt[:CRYPT_BLOCK_SIZE]
+    text_to_encrypt = text_to_encrypt[CRYPT_BLOCK_SIZE:]
+    encrypted_text += key.encrypt(chunk, 32)[0]
+    if len(text_to_encrypt) == 0:
+      break
 
-  if len(args) == 3:
-    write_file(args[2], encoded_text)
-  else:
-    sys.stdout.write(encoded_text)
+  write_file(args[2], encrypted_text)
 
 
 def decrypt(args):
@@ -54,8 +59,14 @@ def decrypt(args):
 
   private_key = read_file(private_key_file)
   key = RSA.importKey(private_key)
-  encoded_text = read_file(source_file)
-  decrypted_text = key.decrypt(ast.literal_eval(_decode(encoded_text)))
+  encrypted_text = read_file(source_file)
+  decrypted_text = ''
+  while True:
+    chunk = encrypted_text[:CRYPT_BLOCK_SIZE]
+    encrypted_text = encrypted_text[CRYPT_BLOCK_SIZE:]
+    decrypted_text += key.decrypt(chunk)
+    if len(encrypted_text) == 0:
+      break
 
   if len(args) == 3:
     write_file(args[2], decrypted_text)
@@ -63,21 +74,15 @@ def decrypt(args):
     sys.stdout.write(decrypted_text)
 
 def read_file(filename):
-  handler = open(filename, "r")
+  handler = open(filename, "rb")
   content = handler.read()
   handler.close()
   return content
 
 def write_file(filename, text):
-  handler = open(filename, "w")
+  handler = open(filename, "wb")
   handler.write(text)
   handler.close()
-
-def _encode(string):
-  return base64.b64encode(string)
-
-def _decode(string):
-  return base64.b64decode(string)
 
 def main():
   arguments = sys.argv
